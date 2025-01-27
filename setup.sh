36c1e3a33c76d6f623846ab386b08816693dc3b6
Alexander Soklev <alex@makepath.com>
2025-01-27 14:21:29 +0200

    cmake: Added all header files to the install step

--- setup.sh
+++ setup.sh
@@ -1,23 +1,27 @@
 rm -rf build
-# conda --version
-# conda env create -f environment-dev.yml
+
+BUILD_DIR=build
+BUILD_TYPE=Release
 
 FC=gfortran
 FFLAGS='-ffree-line-length-none -m64 -std=f2008 -march=native -fbounds-check -fmodule-private -fimplicit-none -finit-real=nan'
+
+BUILD_TESTING=ON
 RTE_ENABLE_SP=OFF
 KERNEL_MODE=default
 FAILURE_THRESHOLD='7.e-4'
 
-cmake -S . -B build -G "Ninja" \
+cmake -S . -B $BUILD_DIR -G "Ninja" \
         -DCMAKE_Fortran_COMPILER=$FC \
         -DCMAKE_Fortran_FLAGS="$FFLAGS" \
         -DRTE_ENABLE_SP=$RTE_ENABLE_SP \
         -DKERNEL_MODE=$KERNEL_MODE \
-        -DBUILD_TESTING=ON \
+        -DBUILD_TESTING=$BUILD_TESTING \
         -DFAILURE_THRESHOLD=$FAILURE_THRESHOLD \
-        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
-# cmake --build build --target all --parallel
+cmake --build $BUILD_DIR --parallel
 
-# The --test-dir option is available only starting CMake 3.20:
-# ctest -V --test-dir build
+cmake --install $BUILD_DIR --prefix install_test
+
+ctest --output-on-failure --test-dir ${BUILD_DIR} -V
